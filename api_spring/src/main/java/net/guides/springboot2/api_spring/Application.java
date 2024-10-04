c92b4c64b5a3e148ea3bd14a57652004f883bc98
Caleb <kalebsam@hotmail.com>
2024-10-03 21:37:26 -0500

    ajustes en db

--- api_spring/src/main/java/net/guides/springboot2/api_spring/Application.java
+++ api_spring/src/main/java/net/guides/springboot2/api_spring/Application.java
@@ -1,10 +1,8 @@
 package net.guides.springboot2.api_spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
